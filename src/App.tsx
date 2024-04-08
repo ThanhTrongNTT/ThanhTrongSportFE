c0b7a3564b2c2afc91647728aa7bb2ca8d5172de
HoangHuy296 <19110218@student.hcmute.edu.vn>
2024-04-08 22:18:27 +0700

    commit 8/4/24
    main layout , const router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import "./App.css";
 import LoginScreen from "./screens/LoginScreen.tsx";
-
+import SignUpPage from "./screens/SignUpPage.tsx";
+import ProductPage from "./screens/ProductPage.tsx";
+import ProductView from "./screens/ProductView.tsx";
+import ShopCart from "./screens/ShopCart.tsx";
 const App = () => {
   // eslint-disable-next-line react/react-in-jsx-scope
-  return <LoginScreen />;
+  return <ShopCart />;
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import Header from "./component/Header/header.tsx";
+import Footer from "./component/Footer/footer.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <Header />
     <App />
-  </React.StrictMode>,
+    <Footer />
+  </React.StrictMode>
 );
