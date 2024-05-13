6e8e5d00cdbd6166a348d29b472860c69a6fc9c6
HoangHuy296 <19110218@student.hcmute.edu.vn>
2024-05-14 01:39:22 +0700

    update

--- src/component/Header/header.tsx
+++ src/component/Header/header.tsx
@@ -24,10 +24,7 @@ const Header = () => {
               />
             </svg>
           </button>
-          <button
-            className="px-4 py-2 text-sm rounded-full font-bold text-white border-2 border-[#007bff] bg-[#007bff] transition-all ease-in-out duration-300 hover:bg-transparent hover:text-[#007bff] ml-3"
-            onClick={LoginScreen}
-          >
+          <button className="px-4 py-2 text-sm rounded-full font-bold text-white border-2 border-[#007bff] bg-[#007bff] transition-all ease-in-out duration-300 hover:bg-transparent hover:text-[#007bff] ml-3">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
